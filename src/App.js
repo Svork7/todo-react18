c27ee10de0f0471ff32106f7d1f806729cf7961d
Eugene Mikhailov <svork@ya.ru>
2022-11-18 16:59:47 +0300

    add input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import TodoForm from '../components/Todos/TodoForm'
-import TodoList from '../components/Todos/TodoList'
+import { useState } from 'react'
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const addTodoHandler = (text) => {
+    setTodos([...todos, text])
+  }
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
-      <TodoForm />
-      <TodoList />
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} />
     </div>
   )
 }
